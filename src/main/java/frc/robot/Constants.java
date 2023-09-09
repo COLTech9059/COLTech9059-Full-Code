0c1d6b0847b0202b58700453eb2bdcdd7c3459a8
COLTech9059 <122689595+COLTech9059@users.noreply.github.com>
2023-09-09 15:20:43 -0700

    Pneumatics Commented Out

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,11 +15,11 @@ package frc.robot;
 public final class Constants {
 
     // HamsterDrive motor controller ports
-    public static final int leftPPort = 2;
-    public static final int rightPPort = 3;
-    public static final int leftFPort = 4;
-    public static final int rightFPort = 5;
-
+    public static final int leftPPort = 1;
+    public static final int rightPPort = 10;
+    public static final int leftFPort = 0;
+    public static final int rightFPort = 11;
+    
     // Define motor controller port for the manipulator subsystem
     public static final int manipulatorMotorPort = 7;
 
